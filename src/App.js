838438005a8054714d1500c362eff7ab1ebebdae
Brosh82-B <brosh82.ploga.b@gmail.com>
2023-11-24 15:16:42 +0200

    .

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import StoreBox from './components/StoreBox/StoreBox';
+import AppFooter from './components/AppFooter/AppFooter';
+import AppHeader from './components/AppHeader/AppHeader';
+import Loading from './components/Loading/Loading';
+function App() {
+  const [storesList, setStoresList] = useState([])
+  useEffect(() => {
+    fetch('https://script.google.com/macros/s/AKfycby4TMg3-pXXr10epmlZCN0nuzvi44IAjW456R9saWjTU19jVXPMz97zpb9FfPLvP4U_/exec')
+      .then((response) => response.json())
+      .then((data) => {
+        setStoresList(data)
+      })
+
+  }, []);
+  return (
+
+    <div className="App">
+      <AppHeader />
+      <div className='main'>
+        {storesList.length !== 0 ? storesList.map(store => {
+          return <StoreBox name={store.Name} description={store.Description} openingTimes={store.OpeningTimes} phoneNumber={store.Phone} key={store.Name} />
+        }) : <Loading />}
+
+      </div>
+      <AppFooter />
+
+
+    </div>
+
+  );
+}
+
+export default App;
